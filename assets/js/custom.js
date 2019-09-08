cf2b06288cbebe40310028409512ee571e22569f
gao-duan <gao-d17@mails.tsinghua.edu.cn>
2019-09-08 17:38:27 +0800

    add navbar && blog

---
+++ assets/js/custom.js
@@ -0,0 +1,43 @@
+let height_all = $('#mvsvbrdf').outerHeight(true);
+let height_title = $('#mvsvbrdf_title').outerHeight(true); // 64
+let height_author = $('#mvsvbrdf_author').outerHeight(true); // 64
+let height_links = $('#mvsvbrdf_links').outerHeight(false); // 38
+let height_desire = ( height_all - height_title - height_author - height_links );
+let options = {
+	height: height_desire,
+	keep: '.toggle',
+};
+let wrappers = document.querySelectorAll( ".element-to-truncate" );
+
+document.addEventListener( "DOMContentLoaded", () => {	
+	for(var i=0;i< wrappers.length;++i){
+		let wrapper = wrappers[i];
+		let dot = new Dotdotdot( wrapper, options );
+		let api = dot.API;
+		wrapper.addEventListener( 'click', ( evnt ) => {
+			if ( evnt.target.closest( '.toggle' ) ) {
+				evnt.preventDefault();
+				//	When truncated, restore
+				if ( wrapper.matches( '.ddd-truncated' ) )
+				{
+					api.restore();
+					wrapper.classList.add( 'full-story' );
+				}
+				//	Not truncated, truncate
+				else
+				{
+					wrapper.classList.remove( 'full-story' );
+					api.truncate();
+					api.watch();
+				}
+			}
+		});
+	}
+});
+
+
+// // for nav jump
+// var height_h3 = $('#title_h3').outerHeight(true) * 0.9;
+// var shiftWindow = function() { scrollBy(0, -height_h3); };
+// if (window.location.hash) shiftWindow();
+// window.addEventListener("hashchange", shiftWindow);
---
+++ blogs/hello_world/hello.md
@@ -0,0 +1,15 @@
+# Hello World!
+
+I decided to set up my own blog for recording ideas in daily life including life and technique.
+
+I have built personal blogs many times (using Hexo, Jekyll, etc), but gave up to maintain on the way. I think the reason is that the whole pipeline is too complex to use. This is not to say these tools are hard to use, `complex` means extra dependencies makes it not simple and elegant.
+
+And this time, I decide to restart my blog with only HTML files (I use Typora to export my articles as HTML file).
+
+There will be mainly 2 kinds of articles:
+
+- Some ideas and experiences of my daily life (mainly using Chinese).
+- Some technique articles about renderings and others. (English & Chinese maybe).
+
+Please feel free to contact me! (email address: gao-d17@mails.tsinghua.edu.cn, gaoduan0306@163.com)
+
